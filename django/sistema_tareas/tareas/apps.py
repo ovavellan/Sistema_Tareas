004eb924b1bf1f6ba52510f7c3d9ce46edfa178a
ovavellan <oscaravellan2000@gmail.com>
2025-01-30 15:36:21 -0500

    Revert "Adición de proyecto de Django y cargas de JMetter"
    This reverts commit 25cac25a302aa7ee57c1967cfb29115f0a214c4d.

--- django/sistema_tareas/tareas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TareasConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "tareas"
-
-    def ready(self):
-        import tareas.signals
